31fb5c90699ba9fcad83b22c41fddde442b4dc79
Kaiming Ouyang <kouya001@ucr.edu>
2021-05-22 14:25:28 -0500

    output all msg results in one run

--- one-stage/cblas-acc-bench-netmod-msg.c
+++ one-stage/cblas-acc-bench-netmod-msg.c
@@ -104,6 +104,9 @@ void init_matrix(double **a, double **b, double **c, int dim, int wcnt) {
 #define WMATRIX_PER_WORKER 9
 #define COMMPROC_PER_NUMA 9
 
+#define MSG_CNT 15
+int msg_size[15] = {16,32,64,128,512,1024,8192,16384,65536,131072,524288,1048576,4194304,8388608,16777216};
+
 int main(int argc, char *argv[])
 {
     int rank, size;
@@ -121,9 +124,9 @@ int main(int argc, char *argv[])
     MPI_Comm inter_node_numa_comm, intra_numa_comm;
     rma_bench_create_comm(&inter_node_numa_comm, &intra_numa_comm);
 
-    int block = atoi(argv[1]); // message size
-    int message_cnt = atoi(argv[2]); // message cnt per communication proc
-    int dim = atoi(argv[3]); // matrix dimension
+    //int block = atoi(argv[1]); // message size
+    int message_cnt = atoi(argv[1]); // message cnt per communication proc
+    int dim = atoi(argv[2]); // matrix dimension
 
     // char hostname[64];
     // gethostname(hostname, 64);
@@ -138,146 +141,154 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(inter_node_numa_comm, &inter_numa_rank);
     int step = COMMPROC_PER_NUMA;
 
-    int actual_message_cnt, buf_sz, actual_wcnt;
-    double *a, *b, *c;
+    for(int iter = 0; iter<MSG_CNT; ++iter) {
+        int actual_message_cnt, buf_sz, actual_wcnt;
+        double *a, *b, *c;
 
-    /* get #messages to be issued */
-    int total_message_cnt = message_cnt * COMMPROC_PER_NUMA;
+        int block = msg_size[iter];
+        /* get #messages to be issued */
+        int total_message_cnt = message_cnt * COMMPROC_PER_NUMA;
 
-    if(intra_numa_rank < step) {
-        int cur_commproc = intra_numa_size / 2;
-        actual_message_cnt = total_message_cnt * (intra_numa_rank + 1) / cur_commproc - total_message_cnt * intra_numa_rank / cur_commproc;
-        buf_sz = block * actual_message_cnt;
-        // printf("rank %d, numa rank %d - total message %d, issue %d\n", rank, intra_numa_rank, total_message_cnt, actual_message_cnt);
-    } else {
-        /* get workload to be done  */
-        int total_wcnt = WMATRIX_PER_WORKER * WORKER_PER_NUMA;
-        int cur_worker = intra_numa_size - step;
-        int wrank = intra_numa_rank - step;
-        actual_wcnt = total_wcnt * (wrank + 1) / cur_worker - total_wcnt * wrank / cur_worker;
+        if(intra_numa_rank < step) {
+            int cur_commproc = intra_numa_size / 2;
+            actual_message_cnt = total_message_cnt * (intra_numa_rank + 1) / cur_commproc - total_message_cnt * intra_numa_rank / cur_commproc;
+            buf_sz = block * actual_message_cnt;
+            // printf("rank %d, numa rank %d - total message %d, issue %d\n", rank, intra_numa_rank, total_message_cnt, actual_message_cnt);
+        } else {
+            /* get workload to be done  */
+            int total_wcnt = WMATRIX_PER_WORKER * WORKER_PER_NUMA;
+            int cur_worker = intra_numa_size - step;
+            int wrank = intra_numa_rank - step;
+            actual_wcnt = total_wcnt * (wrank + 1) / cur_worker - total_wcnt * wrank / cur_worker;
 
-        init_matrix(&a, &b, &c, dim, actual_wcnt);
-        buf_sz = block * (total_message_cnt / step + 1);
-        // printf("rank %d, numa rank %d - total wcnt %d, my wcnt %d\n", rank, intra_numa_rank, total_wcnt, actual_wcnt);
-    }
+            init_matrix(&a, &b, &c, dim, actual_wcnt);
+            buf_sz = block * (total_message_cnt / step + 1);
+            // printf("rank %d, numa rank %d - total wcnt %d, my wcnt %d\n", rank, intra_numa_rank, total_wcnt, actual_wcnt);
+        }
 
-    void *inter_win_ptr;
-    MPI_Win inter_numa_win;
-    MPI_Win_allocate(buf_sz, 1, MPI_INFO_NULL, inter_node_numa_comm, (void *)&inter_win_ptr, &inter_numa_win);
+        void *inter_win_ptr;
+        MPI_Win inter_numa_win;
+        MPI_Win_allocate(buf_sz, 1, MPI_INFO_NULL, inter_node_numa_comm, (void *)&inter_win_ptr, &inter_numa_win);
 
-    memset(inter_win_ptr, BUF_INIT_VALUE, buf_sz);
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    char *inter_local_buf = malloc(buf_sz);
-    memset(inter_local_buf, 0, buf_sz);
-
-    MPI_Win_lock_all(MPI_MODE_NOCHECK, inter_numa_win);
-
-    int num_numa_worker = intra_numa_size - step;
-
-    for (int k = 0; k < WARMUP_ITERATION; ++k)
-    {
+        memset(inter_win_ptr, BUF_INIT_VALUE, buf_sz);
         MPI_Barrier(MPI_COMM_WORLD);
-        if (intra_numa_rank < step) {
-            /* do shared and inter-node rma */
-            int inter_peer = step + intra_numa_rank % num_numa_worker;
-            if (inter_numa_rank < intra_numa_size) {
-                inter_peer += intra_numa_size;
-            }
 
-            for (int i = 0; i < buf_sz; i += block)
-            {
-                //MPI_Get(intra_local_buf + i, block, MPI_CHAR, share_peer, i, block, MPI_CHAR, intra_numa_win);
-                MPI_Accumulate(inter_local_buf + i, block, MPI_CHAR, inter_peer, i, block, MPI_CHAR, MPI_BXOR, inter_numa_win);
-            }
-            //MPI_Win_flush_all(intra_numa_win);
-            MPI_Win_flush_all(inter_numa_win);
-            //assert(check_results(intra_local_buf, inter_local_buf, buf_sz) == 0);
-        }
-        else {
-            /* do dummy compute */
-            for(int i = 0; i < actual_wcnt; ++i)
-                cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, dim, dim, dim, 1.0, a + i * dim * dim, dim, b + i * dim * dim, dim, 1.0, c + i * dim * dim, dim);
-        }
+        char *inter_local_buf = malloc(buf_sz);
+        memset(inter_local_buf, 0, buf_sz);
 
-        // memset(intra_local_buf, 0, buf_sz);
-        // memset(inter_local_buf, 0, buf_sz);
-    }
+        MPI_Win_lock_all(MPI_MODE_NOCHECK, inter_numa_win);
 
-    double time = 0.0;
-    double comm_time = 0.0, cblas_time = 0.0;
-    double max_time;
-    double max_comm, max_cblas;
-    double rec_times[AVE_ITERATION];
-    double rec_comm_times[AVE_ITERATION];
-    double rec_cblas_times[AVE_ITERATION];
+        int num_numa_worker = intra_numa_size - step;
 
-    for (int k = 0; k < AVE_ITERATION; ++k)
-    {
-        MPI_Barrier(MPI_COMM_WORLD);
-        time = MPI_Wtime();
-        if (intra_numa_rank < step)
+        for (int k = 0; k < WARMUP_ITERATION; ++k)
         {
-            /* do shared and inter-node rma */
-            comm_time = MPI_Wtime();
-            int inter_peer = step + intra_numa_rank % num_numa_worker;
-            if (inter_numa_rank < intra_numa_size) {
-                inter_peer += intra_numa_size;
+            MPI_Barrier(MPI_COMM_WORLD);
+            if (intra_numa_rank < step) {
+                /* do shared and inter-node rma */
+                int inter_peer = step + intra_numa_rank % num_numa_worker;
+                if (inter_numa_rank < intra_numa_size) {
+                    inter_peer += intra_numa_size;
+                }
+
+                for (int i = 0; i < buf_sz; i += block)
+                {
+                    //MPI_Get(intra_local_buf + i, block, MPI_CHAR, share_peer, i, block, MPI_CHAR, intra_numa_win);
+                    MPI_Accumulate(inter_local_buf + i, block, MPI_CHAR, inter_peer, i, block, MPI_CHAR, MPI_BXOR, inter_numa_win);
+                }
+                //MPI_Win_flush_all(intra_numa_win);
+                MPI_Win_flush_all(inter_numa_win);
+                //assert(check_results(intra_local_buf, inter_local_buf, buf_sz) == 0);
+            }
+            else {
+                /* do dummy compute */
+                for(int i = 0; i < actual_wcnt; ++i)
+                    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, dim, dim, dim, 1.0, a + i * dim * dim, dim, b + i * dim * dim, dim, 1.0, c + i * dim * dim, dim);
             }
 
-            for (int i = 0; i < buf_sz; i += block)
-            {
-                MPI_Accumulate(inter_local_buf + i, block, MPI_CHAR, inter_peer, i, block, MPI_CHAR, MPI_BXOR, inter_numa_win);
-            }
-            MPI_Win_flush_all(inter_numa_win);
-            //assert(check_results(intra_local_buf, inter_local_buf, buf_sz) == 0);
-            comm_time = MPI_Wtime() - comm_time;
-        }
-        else
-        {
-            /* do dummy compute */
-            cblas_time = MPI_Wtime();
-            for(int i = 0; i < actual_wcnt; ++i)
-                cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, dim, dim, dim, 1.0, a + i * dim * dim, dim, b + i * dim * dim, dim, 1.0, c + i * dim * dim, dim);
-            cblas_time = MPI_Wtime() - cblas_time;
+            // memset(intra_local_buf, 0, buf_sz);
+            // memset(inter_local_buf, 0, buf_sz);
         }
 
-        time = MPI_Wtime() - time;
-        MPI_Reduce(&comm_time, &max_comm, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        MPI_Reduce(&cblas_time, &max_cblas, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        MPI_Reduce(&time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        rec_comm_times[k] = max_comm;
-        rec_cblas_times[k] = max_cblas;
-        rec_times[k] = max_time;
-    }
+        double time = 0.0;
+        double comm_time = 0.0, cblas_time = 0.0;
+        double max_time;
+        double max_comm, max_cblas;
+        double rec_times[AVE_ITERATION];
+        double rec_comm_times[AVE_ITERATION];
+        double rec_cblas_times[AVE_ITERATION];
 
-    MPI_Win_unlock_all(inter_numa_win);
-
-    if (rank == 0)
-    {
-        double ave_time = 0.0, ave_comm_time = 0.0, ave_cblas_time = 0.0;
         for (int k = 0; k < AVE_ITERATION; ++k)
         {
-            ave_time += rec_times[k];
-            ave_comm_time += rec_comm_times[k];
-            ave_cblas_time += rec_cblas_times[k];
-            //printf("Iter %d - total time %.3lf, comm %.3lf, sleep %.3lf\n", k, rec_times[k] * 1e6, rec_comm_times[k] * 1e6, rec_cblas_times[k] * 1e6);
+            MPI_Barrier(MPI_COMM_WORLD);
+            time = MPI_Wtime();
+            if (intra_numa_rank < step)
+            {
+                /* do shared and inter-node rma */
+                comm_time = MPI_Wtime();
+                int inter_peer = step + intra_numa_rank % num_numa_worker;
+                if (inter_numa_rank < intra_numa_size) {
+                    inter_peer += intra_numa_size;
+                }
+
+                for (int i = 0; i < buf_sz; i += block)
+                {
+                    MPI_Accumulate(inter_local_buf + i, block, MPI_CHAR, inter_peer, i, block, MPI_CHAR, MPI_BXOR, inter_numa_win);
+                }
+                MPI_Win_flush_all(inter_numa_win);
+                //assert(check_results(intra_local_buf, inter_local_buf, buf_sz) == 0);
+                comm_time = MPI_Wtime() - comm_time;
+            }
+            else
+            {
+                /* do dummy compute */
+                cblas_time = MPI_Wtime();
+                for(int i = 0; i < actual_wcnt; ++i)
+                    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, dim, dim, dim, 1.0, a + i * dim * dim, dim, b + i * dim * dim, dim, 1.0, c + i * dim * dim, dim);
+                cblas_time = MPI_Wtime() - cblas_time;
+            }
+
+            time = MPI_Wtime() - time;
+            MPI_Reduce(&comm_time, &max_comm, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+            MPI_Reduce(&cblas_time, &max_cblas, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+            MPI_Reduce(&time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+            rec_comm_times[k] = max_comm;
+            rec_cblas_times[k] = max_cblas;
+            rec_times[k] = max_time;
         }
 
-        ave_time = ave_time / AVE_ITERATION;
-        ave_comm_time = ave_comm_time / AVE_ITERATION;
-        ave_cblas_time = ave_cblas_time / AVE_ITERATION;
-        /*
-        for (int k = 0; k < AVE_ITERATION; ++k)
+        MPI_Win_unlock_all(inter_numa_win);
+
+        if (rank == 0)
         {
-            time_devi += (rec_times[k] - ave_time) * (rec_times[k] - ave_time);
+            double ave_time = 0.0, ave_comm_time = 0.0, ave_cblas_time = 0.0;
+            for (int k = 0; k < AVE_ITERATION; ++k)
+            {
+                ave_time += rec_times[k];
+                ave_comm_time += rec_comm_times[k];
+                ave_cblas_time += rec_cblas_times[k];
+                //printf("Iter %d - total time %.3lf, comm %.3lf, sleep %.3lf\n", k, rec_times[k] * 1e6, rec_comm_times[k] * 1e6, rec_cblas_times[k] * 1e6);
+            }
+
+            ave_time = ave_time / AVE_ITERATION;
+            ave_comm_time = ave_comm_time / AVE_ITERATION;
+            ave_cblas_time = ave_cblas_time / AVE_ITERATION;
+            /*
+            for (int k = 0; k < AVE_ITERATION; ++k)
+            {
+                time_devi += (rec_times[k] - ave_time) * (rec_times[k] - ave_time);
+            }
+            time_devi = sqrt(time_devi / AVE_ITERATION);
+            */
+            printf("%d %.3lf %.3lf %.3lf\n", block, ave_time * 1e6, ave_comm_time * 1e6, ave_cblas_time * 1e6);
         }
-        time_devi = sqrt(time_devi / AVE_ITERATION);
-        */
-        printf("%d %.3lf %.3lf %.3lf\n", block, ave_time * 1e6, ave_comm_time * 1e6, ave_cblas_time * 1e6);
+
+        MPI_Win_free(&inter_numa_win);
+        free(inter_local_buf);
+        free(a);
+        free(b);
+        free(c);
     }
 
-    MPI_Win_free(&inter_numa_win);
     MPI_Comm_free(&intra_numa_comm);
     MPI_Comm_free(&inter_node_numa_comm);
     MPI_Finalize();
